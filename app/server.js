d0cdacadb7b347ed814dd02cc357e4e44ea8f9d5
Michael Hunger <github@jexp.de>
2013-11-26 22:38:04 +0100

    added heroku web-port

--- app/server.js
+++ app/server.js
@@ -6,7 +6,13 @@ process.on('uncaughtException', function (err) {
 var express = require('express');
 var fs = require('fs');
 //var checkstatus = require('./checkstatus');
+
 var app = express();
+
+app.configure(function () {
+	app.set('port', process.env.PORT || 3000);
+});
+
 var childProcess = require('child_process')
 var phantomjs = require('phantomjs')
 var binPath = phantomjs.path
@@ -14,9 +20,6 @@ var path = require('path')
 var gistStatusCache = {}
 var CACHE_TIMEOUT_DAYS = 1 //days
 
-app.configure(function () {
-	app.set('port', process.env.PORT || 3000);
-});
 
 app.get('/', function (req, res) {
     var url = req.query.url;
@@ -71,5 +74,6 @@ app.get('/', function (req, res) {
 });
 
 
-app.listen(3000);
-console.log('Listening on port 3000');
+app.listen(app.get('port'), function () {
+    console.log("Express server listening on port " + app.get('port'));
+});
